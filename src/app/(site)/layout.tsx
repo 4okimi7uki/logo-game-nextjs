d575aaef5560a9e7b91242ea1d2c78603fe66113
4okimi7uki <bv16001@shibaura-it.ac.jp>
2025-09-07 18:10:37 +0900

    feat: add layout

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -2,19 +2,24 @@
 import Footer from "@/components/layouts/Footer";
 import Header from "@/components/layouts/Header";
 import { GameControlContext } from "@/context/GameControlContext";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function SiteLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     const [correctCount, setCorrectCount] = useState<number>(0);
     const [isFinish, setIsFinish] = useState<boolean>(false);
+    const [timeLeft, setTimeLeft] = useState<number>(30); // 初期の時間制限（３0秒）
 
     const handleCorrectCount = () => {
         setCorrectCount((pre) => pre + 1);
     };
 
-    const handleFinish = () => {
-        setIsFinish(true);
+    const handleFinish = (flg: boolean = true) => {
+        setIsFinish(flg);
     };
+
+    useEffect(() => {
+        setIsFinish(false);
+    }, []);
 
     return (
         <GameControlContext.Provider
@@ -23,12 +28,14 @@ function SiteLayout({ children }: Readonly<{ children: React.ReactNode }>) {
                 onCorrectCount: handleCorrectCount,
                 isFinish: isFinish,
                 onFinish: handleFinish,
+                timeLeft: timeLeft,
+                onTimer: setTimeLeft,
             }}
         >
-            <div className="w-full h-full flex flex-col justify-stretch">
-                <Header />
-                <main className="h-full">{children}</main>
-                <Footer />
+            <div className="w-full h-full grid grid-rows-[50px_1fr_30px]">
+                <Header className="row-start-1 h-[50px]" />
+                <main className="row-start-2 h-[calc(100svh-80px)] py-4 px-[max(2%,14px)]">{children}</main>
+                <Footer className="row-start-3 h-[30px]" />
             </div>
         </GameControlContext.Provider>
     );
